cb3b6ba2ae1afc2ab9aba73b2a049320238effad
kregarm <martinkregar@gmail.com>
2016-10-15 09:03:30 +0200

    fixed header behaviour

--- electron-app/app/partial/header/header.js
+++ electron-app/app/partial/header/header.js
@@ -1,5 +1,12 @@
-angular.module('app').controller('HeaderCtrl',function($scope, projectService){
+angular.module('app').controller('HeaderCtrl',function($scope, projectService, $state){
 
     $scope.project = projectService.model.item;
+    $scope.title = {};
+
+    if($state.current.name ===  'list.projects' || $state.current.name ===  'list.new-project') {
+        $scope.title.name = '';
+    } else {
+        $scope.title.name = $scope.project.projectName;
+    }
 
 });
